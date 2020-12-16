6478a0c4b08b1427c018f2a6577fe1c5c5882d90
dyatchenko-mv <76127500+dyatchenko-mv@users.noreply.github.com>
2020-12-17 00:46:10 +0300

    Update randproject.py

--- randproject.py
+++ randproject.py
@@ -10,7 +10,27 @@ class RandomCracker:
         self.mt.append(self._harden_inverse(bits))
         if self.counter == 624:
             self._regen()
+            
+    def _to_bitarray(self, num):
+        k = [int(x) for x in bin(num)[2:]]
+        return [0] * (32-len(k)) + k
 
     def _predict(self):
+        if not self.state:
+            print("Didn't recieve enough bits to predict")
+            return 0
         if self.counter >= 624:
             self._regen()
+        self.counter += 1
+
+        return self._harden(self.mt[self.counter-1])
+    
+    # добавлена закалка
+    def _harden(self, bits):
+        bits = self._xor_nums(bits, bits[:-11])
+        bits = self._xor_nums(bits, self._and_nums(bits[7:] + [0] * 7 , self._to_bitarray(0x9d2c5680)))
+        bits = self._xor_nums(bits, self._and_nums(bits[15:] + [0] * 15 , self._to_bitarray(0xefc60000)))
+        bits = self._xor_nums(bits, bits[:-18])
+        return bits
+    
+    
