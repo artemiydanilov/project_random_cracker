09420841c7dc18a950fff99425b4351cf213c0f1
grishanosov <31705594+grishanosov@users.noreply.github.com>
2020-12-17 01:02:58 +0300

    Update randproject.py

--- randproject.py
+++ randproject.py
@@ -59,4 +59,48 @@ class RandomCracker:
         bits = self._xor_nums(bits, bits[:-18])
         return bits
     
+    def _decode_harden_midop(self, enc, and_arr, shift):
+        
+        NEW = 0
+        XOR = 1
+        OK =  2
+        work = []
+        for i in range(32):
+            work.append((NEW,enc[i]))
+        changed = True
+        while changed:
+            changed = False
+            for i in range(32):
+                status = work[i][0]
+                data = work[i][1]
+                if i >= 32-shift and status == NEW:
+                    work[i] = (OK,data)
+                    changed = True
+                elif i < 32-shift and status == NEW:
+                    if and_arr[i] == 0:
+                        work[i] = (OK, data)
+                        changed = True
+                    else:
+                        work[i] = (XOR, data)
+                        changed = True
+                elif status == XOR:
+                    i_other = i+shift
+                    if work[i_other][0] == OK:
+                        work[i] = (OK, data ^ work[i_other][1])
+                        changed = True
+
+        return [x[1] for x in work]
+    
+    def _harden_inverse(self, bits):
+        # inverse for: bits = _xor_nums(bits, bits[:-11])
+        bits = self._xor_nums(bits, bits[:-18])
+        # inverse for: bits = _xor_nums(bits, _and_nums(bits[15:] + [0] * 15 , _to_bitarray(0xefc60000)))
+        bits = self._decode_harden_midop(bits, self._to_bitarray(0xefc60000), 15)
+        # inverse for: bits = _xor_nums(bits, _and_nums(bits[7:] + [0] * 7 , _to_bitarray(0x9d2c5680)))
+        bits = self._decode_harden_midop(bits, self._to_bitarray(0x9d2c5680), 7)
+        # inverse for: bits = _xor_nums(bits, bits[:-11])
+        bits = self._xor_nums(bits, [0] * 11 + bits[:11]+[0] * 10)
+        bits = self._xor_nums(bits, bits[11:21])
+
+        return bits
     
