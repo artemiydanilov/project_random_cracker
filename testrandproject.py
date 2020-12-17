0899d98b5a1ac9d3f9816a15630904efc30abe7c
artemiydanilov <danilov.av@phystech.edu>
2020-12-17 12:30:47 +0300

    add new nests

--- testrandproject.py
+++ testrandproject.py
@@ -28,11 +28,26 @@ class TestRandomCracker(unittest.TestCase):
             self.randomCracker.submit(random.randint(0, 4294967294))
         self.assertTrue(sum([random.randint(0, 4294967294) == self.randomCracker.predict()
                                  for i in range(10000)]) / 100.0 >= 94)
-        
+
+    def test1000(self):
+        random.seed(time.time() + 3)
+        for i in range(624):
+            self.randomCracker.submit(random.randint(0, 4294967294))
+        self.assertTrue(sum([random.randint(0, 4294967294) == self.randomCracker.predict()
+                                 for i in range(1000)]) / 10.0 >= 99)
+
+    def test100000(self):
+        random.seed(time.time() + 2)
+        for i in range(624):
+            self.randomCracker.submit(random.randint(0, 4294967294))
+        self.assertTrue(sum([random.randint(0, 4294967294) == self.randomCracker.predict()
+                                 for i in range(100000)]) / 1000.0 <= 50)
+
     def testXOR(self):
         self.assertEqual(self.randomCracker.xor_nums([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1], 
                                                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1]),
                                                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0])
+
     def testOR(self):
         self.assertEqual(self.randomCracker.or_nums([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1,1],
                                                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,0,0]),
